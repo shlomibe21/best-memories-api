4b3748ff5ad82c812d7f07010b1de2ffc1e394f5
Shlomi <shlomi@onca-inc.com>
2018-09-24 22:44:41 -0700

    Fixed CORS issue.

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const app = express();
 
-const cors = require("cors");
-const { CLIENT_ORIGIN } = require("./config");
-
 const PORT = process.env.PORT || 3000;
 
 app.get("/api/*", (req, res) => {
   res.json({ ok: true });
 });
 
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
-);
+// CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    if (req.method === 'OPTIONS') {
+        return res.send(204);
+    }
+    next();
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
