9703d1c5c122a04e788992f19ab89c03c1d9f643
Shlomi <shlomi@onca-inc.com>
2018-09-28 12:06:59 -0700

    1. Added mongo/mongoose support. 2. Added most important endpoints and tests for these endpoints.

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+'use strict'
+
+require('dotenv').config();
 const express = require("express");
+const mongoose = require('mongoose');
+
+// Mongoose internally uses a promise-like object,
+// but its better to make Mongoose use built in es6 promises
+mongoose.Promise = global.Promise;
+
+const { DATABASE_URL, PORT } = require('./config');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-app.get("/api/*", (req, res) => {
-  res.json({ ok: true });
-});
+const { router: albumsRouter } = require('./albums');
+const bodyParser = require('body-parser');
 
 // CORS
 app.use(function (req, res, next) {
@@ -18,6 +25,68 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
-module.exports = { app };
+app.use('/api/albums/', albumsRouter);
+
+app.get("/api/*", (req, res) => {
+  res.json({ ok: true });
+});
+
+// catch-all endpoint if client makes request to non-existent endpoint
+app.use('*', function (req, res) {
+    res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function( req, res, next, error) {
+    res.status(500).json({ message: 'Internal server error' });
+});
+
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it in run
+let server;
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl, port = PORT) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, { useNewUrlParser: true }, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
+    });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+    runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { runServer, app, closeServer };
