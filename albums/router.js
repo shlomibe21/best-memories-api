5ff0d65776b6242b56ec3f64d0466c417cd6d3d3
Shlomi <shlomi@onca-inc.com>
2018-10-28 19:34:10 -0700

    Changed the end point to upload files to s3, trying to solve a problem on Heroku.

--- albums/router.js
+++ albums/router.js
@@ -29,7 +29,7 @@ router.get("/sign-s3", (req, res) => {
   const s3Params = {
     Bucket: S3_BUCKET,
     Key: `${fileName}`,
-    Expires: 600,
+    //Expires: 600,
     ACL: "public-read",
     ContentType: fileType
   };
@@ -39,12 +39,16 @@ router.get("/sign-s3", (req, res) => {
       console.log(err);
       return res.end();
     }
-    const returnData = {
+    res.json({
+      signedRequest: data,
+      url: `${S3_URL}${fileName}`
+    });
+    /*const returnData = {
       signedRequest: data,
       url: `${S3_URL}${fileName}`
     };
     res.write(JSON.stringify(returnData));
-    res.end();
+    res.end();*/
   });
 });
 
